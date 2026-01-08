19cf1b71a7156c531ac5484460a54bd637ca9707
Amber <68829832+Amberarch@users.noreply.github.com>
2026-01-08 18:34:29 -0500

    finished destroyer lists; TODO: DD scripts

---
+++ scripts/auto.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+export KCCACHEDIR="/home/amber/.config/KCCacheProxy/ProxyData/cache/kcs2/resources/ship"
+export KCBANNERDIR=$KCCACHEDIR/banner
+export KCCARDDIR=$KCCACHEDIR/card
+export ROOTDIR=${PWD%/*}
+export SCRIPTDIR=$PWD
+export WORKDIR=$ROOTDIR/assetWorkDir
+export DESTDIR=$ROOTDIR/garbage
+export MODDIR=$ROOTDIR/garbage
+
+function copy_banner(){
+    export DESTDIR=$WORKDIR/banner
+    cd $KCCACHEDIR
+    $SCRIPTDIR/echo.sh
+    #Common assets
+    bash $ROOTDIR/fileLists/DDcommon
+    #TODO: imagemagick
+    #$SCRIPTDIR/orig.sh
+    #$SCRIPTDIR/move-patched.sh
+
+}
+
+copy_banner
+export DESTDIR=$ROOTDIR/garbage
+export MODDIR=$ROOTDIR/garbage
+$SCRIPTDIR/echo.sh
+
+
