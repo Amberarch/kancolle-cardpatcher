322b31614e2e0e90cc8ffde3533663b1b43b9f76
Amber <68829832+Amberarch@users.noreply.github.com>
2026-01-23 17:34:07 -0500

    DDholo

--- scripts/auto.sh
+++ scripts/auto.sh
@@ -7,27 +7,47 @@ export ROOTDIR=${PWD%/*}
 export SCRIPTDIR=$PWD
 export MODDIR=$ROOTDIR/BannerMod/BannerMod/kcs2/resources/ship
 
-function copy_banner(){
+function copy_banner_DD(){
     cd $KCCBANNERDIR
     # DD common
     while read p;
 	do cd $KCBANNERDIR;pname=$(find -name $p*)
 	   mkdir -p $MODDIR/banner/$pname
 	   cp $pname $MODDIR/banner/$pname/original.png
-	   ffmpeg -hide_banner -y -i $MODDIR/banner/$pname/original.png -i $ROOTDIR/assets/common-badge.png -filter_complex '[0:v]scale=240x60[bak];[1:v]scale=62x60[badge];[bak][badge] overlay=x=-1 [comb]; [comb]drawtext=text=DD:fontsize=30:fontcolor=white:bordercolor=black:borderw=2:y=(h-text_h)/2:x=8' $MODDIR/banner/$pname/patched.png;
-    done <$SCRIPTDIR/generated/DDcommon.csv
+
+	   ffmpeg -hide_banner -y -i $MODDIR/banner/$pname/original.png -i $ROOTDIR/assets/common-badge.png -filter_complex \
+		'[0:v]scale=240x60[bak];
+	   	[1:v]scale=62x60[badge];
+		[bak][badge] overlay=x=-1 [comb];
+		[comb]drawtext=text=DD:fontsize=30:fontcolor=white:bordercolor=black:borderw=2:y=(h-text_h)/2:x=8'\
+	   $MODDIR/banner/$pname/patched.png;
+    done <$ROOTDIR/fileLists/DDcommon.csv
     # DD rare
     while read p;
 	do cd $KCBANNERDIR;pname=$(find -name $p*)
 	   mkdir -p $MODDIR/banner/$pname
 	   cp $pname $MODDIR/banner/$pname/original.png
-	   ffmpeg -hide_banner -y -i $MODDIR/banner/$pname/original.png -i $ROOTDIR/assets/rare-badge.png -filter_complex '[0:v]scale=240x60[bak];[1:v]scale=66x71[badge];[bak][badge] overlay=x=-3:y=-5 [comb]; [comb]drawtext=text=DD:fontsize=30:fontcolor=white:bordercolor=black:borderw=2:y=(h-text_h)/2:x=7' $MODDIR/banner/$pname/patched.png;
-    done <$SCRIPTDIR/generated/DDrare.csv
 
+	   ffmpeg -hide_banner -y -i $MODDIR/banner/$pname/original.png -i $ROOTDIR/assets/rare-badge.png -filter_complex \
+		'[0:v]scale=240x60[bak];
+	   	[1:v]scale=66x71[badge];
+		[bak][badge] overlay=x=-3:y=-5 [comb];
+		[comb]drawtext=text=DD:fontsize=30:fontcolor=white:bordercolor=black:borderw=2:y=(h-text_h)/2:x=7'\
+	   $MODDIR/banner/$pname/patched.png;
+	done < <(cat $ROOTDIR/fileLists/DDrare.csv $ROOTDIR/fileLists/DDvrare.csv)
+    # DD holo
+    while read p;
+	do cd $KCBANNERDIR;pname=$(find -name $p*)
+	   mkdir -p $MODDIR/banner/$pname
+	   cp $pname $MODDIR/banner/$pname/original.png
 
-    #  ffmpeg -i 0001_2910.png -i common-badge.png -filter_complex '[0:v]scale=240x60[bak];[1:v]scale=62x60[badge];[bak][badge] overlay [comb]; [comb]drawtext=text=DD:fontsize=30:fontcolor=white:bordercolor=black:borderw=2:y=(h-text_h)/2:x=10' out.png
-
-#      ffmpeg -i 0122_4216.png -i rare-badge.png -filter_complex '[0:v]scale=240x60[bak];[1:v]scale=66x71[badge];[bak][badge] overlay=x=-3:y=-5 [comb]; [comb]drawtext=text=DD:fontsize=30:fontcolor=white:bordercolor=black:borderw=2:y=(h-text_h)/2:x=7' out.png
+	   ffmpeg -hide_banner -y -i $MODDIR/banner/$pname/original.png -i $ROOTDIR/assets/holo-badge.png -filter_complex \
+		'[0:v]scale=240x60[bak];
+	   	[1:v]scale=71x70[badge];
+		[bak][badge] overlay=x=-5:y=-5 [comb];
+	   	[comb]drawtext=text=DD:fontsize=30:fontcolor=white:bordercolor=black:borderw=2:y=(h-text_h)/2:x=8'\
+	   $MODDIR/banner/$pname/patched.png;
+    done <$ROOTDIR/fileLists/DDholo.csv
 }
 
-copy_banner
+copy_banner_DD
